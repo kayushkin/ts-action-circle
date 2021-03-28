344b85183234f13e941e56519310dadf43e17a2f
Mary Rankin <mary-rankin@outlook.com>
2021-03-27 21:00:40 -0400

    began Move class

--- src/movement.ts
+++ src/movement.ts
@@ -1,81 +1,5 @@
-import {Vec2D} from "./vecs"
-
-
-
-// export interface AutoMove {
-//     autoMove: Move
-//   }
-
-// export type MoveName = keyof typeof _moves
-  
-// let _moves: {[key: string]: Move} = {
-//     NotMoving: {
-//       name: "not moving",
-//       priority: 10,
-//       speed: 0,
-//       direction: Vec2D.default(),
-//       time: 0
-//     },
-//     ArrowKeyMove: {
-//       name: "arrow key move",
-//       priority: 10,
-//       speed: 1,
-//       direction: Vec2D.default(),
-//       time: 1000
-//     },
-//     BasicFire: {
-//       name: "bullet move",
-//       priority: 10,
-//       direction: Vec2D.default(),
-//       speed: 0.5,
-//       time: 10000
-//    },
-//    PushObj: {
-//       name: "collision",
-//       priority: 5,
-//       speed: 1,
-//       direction: Vec2D.default(),
-//       time:5
-//    },
-//    BulletKB: {
-//     name: "bulletKB", 
-//     priority: 2,
-//     speed: 2,  
-//     direction: Vec2D.default(), 
-//     time: 1
-//     },
-//     Dash: {
-//       name: "dash", 
-//       priority: 8,
-//       speed: 1, 
-//       direction: Vec2D.default(), 
-//       time: 250
-//     },
-//     OrangeFire: {
-//       name: "orangeBulletMove",
-//       priority: 5,
-//       speed: 0.5,
-//       direction: Vec2D.default(),
-//       time: 5000
-//     },
-//     Grab: {
-//       name: "grabMove",
-//       priority: 2,
-//       speed: 1.75,
-//       direction: Vec2D.default(),
-//       time: 5000
-//     }
-//   }
-
-// export const moves: { [name in MoveName]: Move } = _moves
-
-// export interface Move {
-//     name: String
-//     priority: number
-//     speed: number
-//     direction: Vec2D
-//     time: number
-//   }
+import { Vec2D } from "./vecs"
+import { Fighter, Bullet } from "./circles"
 
 export type moveName = 'notMoving' | 'arrowKey' | 'basicFire' | 'pushObj' | 'bulletKB' | 'dash' | 'orangeFire' | 'grab'
 
@@ -85,4 +9,21 @@ export abstract class Move {
   constructor(public name: moveName, public priority: number, public speed: number, public time: number) {
     this.direction = Vec2D.default()
   }
+
+  queueMove(fighter: Fighter, attemptedMove: Move) {
+    if (fighter.movement.time < 1 || fighter.movement.priority > (attemptedMove.priority - 1)) {
+      fighter.movement = attemptedMove 
+    }
+  }
+
+  circleMove(circleWithMove: Fighter | Bullet, dt: number) {
+    if (circleWithMove.movement.time >= 0 ){
+      //change distance moved to distance*frames passed or time left
+      let adjustedDistance = circleWithMove.movement.speed*(Math.min(dt, circleWithMove.movement.time))
+      let dcircleWithMovePosn = circleWithMove.movement.direction.clone().scale(adjustedDistance)
+      circleWithMove.posn = circleWithMove.posn.add(dcircleWithMovePosn)
+      circleWithMove.movement.time -= dt
+    }
+  }
+
 }
